4b4064b77e557c7d9787a4980a7f30b2a331a75d
VadimYakovliev <vadim.yakovliev@gmail.com>
2017-10-13 16:05:20 +0300

    Added GM markers

--- Sources/Controllers/LocationViewController.swift
+++ Sources/Controllers/LocationViewController.swift
@@ -13,15 +13,14 @@ import GoogleMaps
 class LocationViewController: UIViewController {
     
     @IBOutlet weak var mapView: GMSMapView?
-
     
     let locationManager = CLLocationManager()
+    var markersArray = [GMSMarker]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //        setupBanner()
-        
         self.setupLocationManager()
     }
     
@@ -35,40 +34,43 @@ class LocationViewController: UIViewController {
         if result != nil {
             for boozeUp in result! {
                 if let drunkParty = BoozeUp.init(json: boozeUp as! JSON) {
-//                    DispatchQueue.main.async {
-                        self.setupMapMarker(latitude: drunkParty.latitude, longitude: drunkParty.longitude, drink: drunkParty.drink)
-//                    }
-                    print("======= Unwrap dictionaties: \(String(describing: drunkParty))")
+                    let marker = setupMapMarker(latitude: drunkParty.latitude, longitude: drunkParty.longitude, drink: drunkParty.drink)
+                    markersArray = [marker]
+//                    print("======= Unwrap dictionaties: \(String(describing: drunkParty))")
                 }
+            }
+            print("======= Objects in markersArray: \(String(describing: markersArray))")
+            for marker in markersArray {
+               marker.map = mapView
             }
         }
     }
     
-    func setupMapMarker(latitude: String?, longitude: String?, drink: Int?) {
-        
-        var curLatitude = 0.0
-        var curLongitude = 0.0
-        
-        if let lat = latitude {
-            curLatitude = Double(lat) ?? 0.0
-        }
-        if let long = longitude {
-            curLongitude = Double(long) ?? 0.0
-        }
+    func setupMapMarker(latitude: String?, longitude: String?, drink: Int?) -> GMSMarker {
         
         let drink = String(describing: drink)
         let markerImage = (BoozeUpIconManager.init(rawValue: drink).image).withRenderingMode(.automatic)
         
-        let marker = GMSMarker(position: CLLocationCoordinate2D(latitude: curLatitude, longitude: curLongitude))
+        let marker = GMSMarker(position: CLLocationCoordinate2D(latitude: stringToDouble(string: latitude), longitude: stringToDouble(string: longitude)))
+        marker.tracksInfoWindowChanges = true
         marker.icon = markerImage
-        marker.map = mapView
+        
+        return marker
+    }
+    
+    func stringToDouble(string: String?) -> Double {
+        var newValue = 0.0
+        
+        if let lat = string {
+            newValue = Double(lat) ?? 0.0
+        }
+        return newValue
     }
     
     @IBAction func checkInButton(_ sender: Any) {
-    
+        
     }
 }
-
 
 // MARK: - AppodealBannerDelegate
 extension LocationViewController: AppodealBannerDelegate {
@@ -81,28 +83,25 @@ extension LocationViewController: AppodealBannerDelegate {
 // MARK: - CLLocationManagerDelegate
 extension LocationViewController: CLLocationManagerDelegate {
     func setupLocationManager() {
-        self.locationManager.delegate = self
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest // Use the highest-level of accuracy
-        self.locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // Use the highest-level of accuracy
+        locationManager.requestWhenInUseAuthorization()
     }
     
     // This function is called when the user grants or does not grant you the right to determine its location
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
             
-            self.locationManager.startUpdatingLocation()
-            
-            self.mapView?.isMyLocationEnabled = true
-            self.mapView?.settings.myLocationButton = true
+            mapView?.isMyLocationEnabled = true
+            mapView?.settings.myLocationButton = true
         }
     }
     
     // When update a geographic position, this delegate's method is called:
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
-            
             self.mapView?.camera = GMSCameraPosition(target: location.coordinate, zoom: 17, bearing: 0, viewingAngle: 0)
-            
             self.locationManager.stopUpdatingLocation()
         }
     }
